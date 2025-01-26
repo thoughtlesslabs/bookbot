2357b65f81a129bec703eaf9615ed836bc015f7c
Thoughtless Labs <thoughtlesslabsinc@gmail.com>
2025-01-25 22:56:53 -0500

    book bot complete

--- main.py
+++ main.py
@@ -2,7 +2,28 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    char_count = get_char_count(text)
+
+    print(char_count)
+    print(f"""
+--- Begin report of {book_path} ---
+{num_words} words found in the document
+    """)
+    for i in char_count:
+        print(f"The '{i}' character was found {char_count[i]} times")
+    print("--- End report ---")
+
+def get_char_count(text):
+    letter_count = {}
+    for char in text:
+        lowercase = char.lower()
+        if lowercase.isalpha():
+            if lowercase in letter_count:
+                letter_count[lowercase] += 1
+            else:
+                letter_count[lowercase] = 1
+    return letter_count
+        
 
 
 def get_num_words(text):
