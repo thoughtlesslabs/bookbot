ad49699fdb6901df382ed06ced36605a289f4ec2
Thoughtless Labs <thoughtlesslabsinc@gmail.com>
2025-03-20 10:51:25 -0400

    Fix

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import get_num_words
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -25,10 +27,6 @@ def get_char_count(text):
     return letter_count
         
 
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
 
 
 def get_book_text(path):
