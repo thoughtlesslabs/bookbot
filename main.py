56f27b1f3c90905ff04d183c638c2a95d0131574
Thoughtless Labs <thoughtlesslabsinc@gmail.com>
2025-03-20 16:40:51 -0400

    Fix

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import sys
 from stats import get_num_words
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_count = get_char_count(text)
@@ -12,7 +16,7 @@ def main():
 {num_words} words found in the document
     """)
     for i in char_count:
-        print(f"The '{i}' character was found {char_count[i]} times")
+        print(f"{i}: {char_count[i]}")
     print("--- End report ---")
 
 def get_char_count(text):
@@ -26,9 +30,6 @@ def get_char_count(text):
                 letter_count[lowercase] = 1
     return letter_count
         
-
-
-
 def get_book_text(path):
     with open(path) as f:
         return f.read()
